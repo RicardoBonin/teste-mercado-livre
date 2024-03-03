89b2bab1a42317da33836bcd42937fb58da3045b
Ricardo Bonin <ricardo.bonin.rb@gmail.com>
2024-03-03 14:44:40 -0300

    feat: add Breadcrumb itens

--- src/Components/Breadcrumb/index.tsx
+++ src/Components/Breadcrumb/index.tsx
@@ -1,12 +1,43 @@
+'use client';
+
 import { memo } from 'react';
 import Link from 'next/link';
+import { AiOutlineRight } from 'react-icons/ai';
+import { useSearchParams } from 'next/navigation';
+import cn from 'classnames';
 
 import styles from './styles.module.css';
 
-function Breadcrumb() {
+interface BreadcrumbsProps {
+  categories: string[];
+}
+
+function Breadcrumb({ categories }: BreadcrumbsProps) {
+  const searchParams = useSearchParams();
+  const search = searchParams.get('search') as string;
+  const cleanCategories = [...new Set(categories.concat(search))];
+
   return (
     <nav className={styles.breadcrumbWrapper}>
-      <Link href="/">Início</Link>
+      <Link className={styles.breadcrumbItem} href="/">
+        Início
+      </Link>
+      {cleanCategories?.map((category, index) => (
+        <div
+          key={category}
+          className={cn(styles.breadcrumbItem, styles.breadcrumbItemMap)}
+        >
+          <AiOutlineRight className={styles.arrowIcon} />
+          <Link
+            className={cn(styles.breadcrumbItem, {
+              [styles.breadcrumbLastItem]: index === cleanCategories.length - 1,
+            })}
+            href={`/items?search=${encodeURIComponent(category)}`}
+          >
+            {category}
+          </Link>
+        </div>
+      ))}
     </nav>
   );
 }
