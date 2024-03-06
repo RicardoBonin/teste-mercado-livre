6ad6ae6ce15f6f8ce1d838034b4dfc17fb85eeda
Ricardo Bonin <ricardo.bonin.rb@gmail.com>
2024-03-06 00:07:30 -0300

    test: create test to home

--- src/Components/Main/index.tsx
+++ src/Components/Main/index.tsx
@@ -5,7 +5,11 @@ import styles from './styles.module.css';
 type MainType = HTMLAttributes<HTMLElement>;
 
 function Main({ children }: MainType) {
-  return <main className={styles.main}>{children} </main>;
+  return (
+    <main data-testid="main" className={styles.main}>
+      {children}{' '}
+    </main>
+  );
 }
 
 export default memo(Main);
