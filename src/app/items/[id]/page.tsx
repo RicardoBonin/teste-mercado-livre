74640a703357cc0f67e8c9df07dcef906fe37daa
Ricardo Bonin <ricardo.bonin.rb@gmail.com>
2024-03-06 00:06:51 -0300

    feat: create test to items id page

--- src/app/items/[id]/page.tsx
+++ src/app/items/[id]/page.tsx
@@ -27,7 +27,9 @@ export default async function Page({ params }: { params: { id: string } }) {
   );
 
   if (!responseProduct.ok) {
-    throw new Error('Failed to fetch data');
+    return {
+      notFound: true,
+    };
   }
 
   const dataProduct = await responseProduct?.json();
